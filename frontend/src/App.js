64ac42f82a8baf2278448aa918dc8440dff202d5
daiki <dahuizhusong@gmail.com>
2025-03-20 18:40:45 +0900

    analytics追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import TopicDetail from "./pages/TopicDetail";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag('config', 'G-KV0MNX21RT', {
+      page_path: location.pathname,
+    });
+  }, [location]);
+
   return (
     <Router>
       <Routes>
