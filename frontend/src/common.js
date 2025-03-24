6c4be0a82a91f5c7d5a46921005e6cb3639c4174
daiki <dahuizhusong@gmail.com>
2025-03-25 08:12:19 +0900

    環境設定すぐやる

--- frontend/src/common.js
+++ frontend/src/common.js
@@ -1,2 +1,3 @@
-export const API_HOST = `https://map-sns-api-dev.onrender.com`;
+// export const API_HOST = `https://map-sns-api-dev.onrender.com`;
+export const API_HOST = `https://map-sns-api.onrender.com`;
 // export const API_HOST = `http://localhost:8000`;
